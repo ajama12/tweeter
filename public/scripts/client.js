cc67b6fd952d1f15985f0f5b6de8dd467d867ee3
Asma Jama <asmajama98@gmail.com>
2023-05-06 19:56:56 -0400

    Fix code so styled error messages appear correctly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,123 +3,113 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(() => {
-  
-  //make template
-    const createTweetElement = (tweet) => {
-      const escape = function (str) {
-        let div = document.createElement("div");
-        div.appendChild(document.createTextNode(str));
-        return div.innerHTML;
-      };  
-      return `
-      <div class="tweet-container">
-        <header class="tweet-header"> 
-          <span class="left-tweet-container"> 
-            <img class="userImg" src=${tweet.user.avatars}/>
-            <span class="userName">
-              ${tweet.user.name}
-            </span> 
-          </span>
-          <span class="userId"> 
-            ${tweet.user.handle}
-          </span>
-        </header>
-        <p class="tweetInfo">
-        ${escape(tweet.content.text)}
-        </p>
-        <footer class="tweet-footer">
-          <span class="date">
-            ${timeago.format(tweet.created_at)}
-          </span>
-          <span>
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </span>
-        </footer>
-      </div>
-    `;
-    };
-    const tweetsContainer = $(".tweets-container");
-  
-  //make tweetbox for tweet
-    const renderTweets = (data) => {
-      tweetsContainer.empty();
-      data.reverse();
-      data.forEach((tweet) => {
-        tweetsContainer.append(createTweetElement(tweet));
-      });
-    };
-  
-    const textArea = $("#tweet-text");
-  
-  //submit tweet
-    const submitNewTweet = (textArea) => {
-      const config = {
-        method: "POST",
-        url: "/tweets",
-        data: textArea.serialize(),
-        success: (tweet) => {
-          console.log("Successfully submitted tweet!", tweet);
-          loadTweet();
-          textArea.val("");
-        },
-        error: (error) => {
-          console.log("Error", error);
-        },
-      };
-      $.ajax(config);
-    };
-  
-    const newErrorMsg = (message) => {
-      return `
-        <p class="error">
-          <i class="fa-solid fa-triangle-exclamation"></i></span>
-          <span class="message">${message}</span>
-          <i class="fa-solid fa-triangle-exclamation"></i>
-        </p>
-      `;
+  const createTweetElement = (tweet) => {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
     };
 
-  //submit new tweet
-    $("form").submit((event) => {
-      event.preventDefault();
-      if (textArea.val() === null || textArea.val() === "") {
-        const errorMsg = newErrorMsg("Sorry, please enter text to tweet!");
-      $(".errorContainer").html(errorMsg).slideDown();
-      } else if (textArea.val().length > 140) {
-        const errorMsg = newErrorMsg("Sorry, this tweet is too long!");
-      $(".errorContainer").html(errorMsg).slideDown();
-      } else if (textArea.val().trim() === "") {
-        const errorMsg = newErrorMsg("Sorry, your tweet can't be blank!");
-      $(".errorContainer").html(errorMsg).slideDown();
-      } else {
-        $(".errorContainer").hide();
-        submitNewTweet(textArea);
-        $(".counter").val(140);
-      }
+    return `
+    <div class="tweet-container">
+      <header class="tweet-header">
+        <span class="left-tweet-container">
+          <img class="userImg" src=${tweet.user.avatars}/>
+          <span class="userName">
+            ${tweet.user.name}
+          </span>
+        </span>
+        <span class="userId">
+          ${tweet.user.handle}
+        </span>
+      </header>
+      <p class="tweetInfo">
+      ${escape(tweet.content.text)}
+      </p>
+      <footer class="tweet-footer">
+        <span class="date">
+          ${timeago.format(tweet.created_at)}
+        </span>
+        <span>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </span>
+      </footer>
+    </div>
+    `;
+  };
+
+  const tweetsContainer = $(".tweets-container");
+
+  const renderTweets = (data) => {
+    tweetsContainer.empty();
+    data.reverse();
+    data.forEach((tweet) => {
+      tweetsContainer.append(createTweetElement(tweet));
     });
-  
-  //render tweets into boxes
-    const loadTweet = () => {
-      const config = {
-        method: "GET",
-        url: "/tweets",
-        success: (tweets) => {
-          console.log("Successfully retrieved!", tweets);
-          renderTweets(tweets);
-        },
-        error: (error) => {
-          console.log("Error", error);
-        },
-      };
-      $.ajax(config);
+  };
+
+  const showError = (errorMessage) => {
+    const errorMsg = `
+      <p class="error">
+        <i class="fa-solid fa-triangle-exclamation"></i></span>
+        <span class="message">${errorMessage}</span>
+        <i class="fa-solid fa-triangle-exclamation"></i>
+      </p>
+    `;
+    $(".errorContainer").html(errorMsg).slideDown();
+
+    setTimeout(() => {
+      $(".errorContainer").slideUp();
+    }, 5000);
+  };
+
+  $("form").submit((event) => {
+    event.preventDefault();
+
+    const textArea = $("#tweet-text");
+    const tweetText = textArea.val().trim();
+
+    if (tweetText === "" || tweetText.length > 140) {
+      const errorMessage = tweetText === "" ? "Sorry, your tweet can't be blank!" : "Sorry, your tweet is over 140 characters!";
+      showError(errorMessage);
+      return;
+    }
+
+    const config = {
+      method: "POST",
+      url: "/tweets",
+      data: textArea.serialize(),
+      success: (tweet) => {
+        console.log("Successfully submitted tweet!", tweet);
+        loadTweet();
+        textArea.val("");
+        $(".counter").text(140);
+      },
+      error: (error) => {
+        console.log("Error", error);
+      },
     };
-  
-  //load preexisting tweets
-    loadTweet();
-    console.log(Date.now());
-  
+    $.ajax(config);
   });
+
+  const loadTweet = () => {
+    const config = {
+      method: "GET",
+      url: "/tweets",
+      success: (tweets) => {
+        console.log("Successfully retrieved!", tweets);
+        renderTweets(tweets);
+      },
+      error: (error) => {
+        console.log("Error", error);
+      },
+    };
+    $.ajax(config);
+  };
+
+  loadTweet();
+  console.log(Date.now());
+});
